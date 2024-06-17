0a9d8f9d43a2de5c10608886630887fdfb9e6141
Rubens <paulo-rolim10@hotmail.com>
2024-06-17 15:32:20 -0300

    Aprendendo sobre API

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Qual filme você quer pesquisar? ");
+        var filme = sc.nextLine();
+
+        var endereco = "https://www.omdbapi.com/?t=" + filme + "&apikey=53fd5cbd";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        sc.close();
+    }
+}
