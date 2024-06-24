9e5e6ab7e1a573fd7585d32534f149f0ff082de1
Rubens <paulo-rolim10@hotmail.com>
2024-06-24 15:36:31 -0300

    Utilizando o Serialized para capturar valor da chave no JSON

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,39 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Qual filme você quer pesquisar? ");
+        var filme = sc.nextLine();
+
+        var endereco = "https://www.omdbapi.com/?t=" + filme + "&apikey=53fd5cbd";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meuFilme = gson.fromJson(json, Titulo.class);
+        System.out.println(meuFilme);
+
+        sc.close();
+    }
+}
